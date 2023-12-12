7c0629f777b114b9bcb3669694d99e91c3f1907a
mdshahjhan <mdshahjha478877@gmail.com>
2023-12-12 13:20:49 +0600

    comunt

--- src/Compnent/Industry/Industry.jsx
+++ src/Compnent/Industry/Industry.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Industryshow from "./Industryshow";
 import { Link } from "react-router-dom";
+import SearchBar from "../SearchBar";
 
 
 const Industry = () => {
@@ -26,26 +27,7 @@ const Industry = () => {
     return (
         <div>
             {/* search bar */}
-            <div className="flex items-center justify-center">
-                <fieldset className="w-full space-y-1 dark:text-gray-100">
-                    <label htmlFor="Search" className="hidden">Search</label>
-                    <div className="relative">
-                        <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-                            <button type="button" title="search" className="p-1 focus:outline-none focus:ring bg-blue-600 text-white">
-                                <svg fill="currentColor" viewBox="0 0 512 512" className="w-4 h-4 dark:text-gray-100">
-                                    {/* SVG path */}
-                                </svg>
-                            </button>
-                        </span>
-                        <input
-                            type="search"
-                            name="Search"
-                            placeholder="Search..."
-                            className="w-32 py-2 pl-10 text-sm rounded-md sm:w-auto focus:outline-none dark:bg-gray-800 dark:text-gray-100 focus:dark:bg-gray-900 focus:dark:border-violet-400"
-                        />
-                    </div>
-                </fieldset>
-            </div>
+            <SearchBar></SearchBar>
 
             {/* Dropdwon  */}
             <div className='flex items-center justify-center'>
---
+++ src/Compnent/SearchBar.jsx
@@ -0,0 +1,34 @@
+// SearchBar.js
+
+
+const SearchBar = ({ onSearch, }) => {
+  return (
+    <div className="flex items-center justify-center mb-9">
+      <div className="relative">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="w-64 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+          onChange={(e) => onSearch(e.target.value)}
+        />
+        <svg
+          className="absolute right-3 top-3 h-5 w-5 text-gray-400"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          ></path>
+        </svg>
+      </div>
+    
+    </div>
+  );
+};
+
+export default SearchBar;
